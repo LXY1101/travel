30f05c095819848d78c716fdcbe3f04a3577bcdb
LXY1101 <1532742044@qq.com>
2020-05-25 18:24:38 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import store from './store'
+import fastClick from 'fastClick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'styles/border.css'
+import 'styles/reset.css'
+import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="swiper">
+    <swiper :options="swiperOption" v-if="showSwiper">
+      <swiper-slide v-for="item of list" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line
+  name: "HomeSwiper",
+  props: {
+    list: Array
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      }
+    }
+  },
+  computed: {
+    showSwiper () {
+      return this.list.length
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.swiper >>> .swiper-pagination-bullet-active
+  background #fff
+.swiper
+  width 100%
+  padding-bottom 31.25%
+  height 0
+  overflow hidden
+  background #eee
+  .swiper-img
+    width 100%
+</style>
